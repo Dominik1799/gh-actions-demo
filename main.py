bf0f059c5e11b5cf46377cc174df9953e71edf28
Dominik Horvath <dom.horvath17@gmail.com>
2022-06-07 12:41:24 +0200

    another one

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
+from logic import get_today
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", date=get_today())
 
 
 

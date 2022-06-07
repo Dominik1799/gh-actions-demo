ead1e82a47c48664c06b1b823958c082aeff98fe
Dominik Horváth <32178114+Dominik1799@users.noreply.github.com>
2022-06-07 12:45:59 +0200

    Update test_sample.py

--- test/test_sample.py
+++ test/test_sample.py
@@ -6,6 +6,6 @@ from datetime import date
 
 class SampleTest(unittest.TestCase):
     def test_today(self):
-        correct_day = date.today().strftime("%d/%m/%Y88")
+        correct_day = date.today().strftime("%d/%m/%Y")
         estimated_day = get_today()
         self.assertEqual(correct_day, estimated_day)
